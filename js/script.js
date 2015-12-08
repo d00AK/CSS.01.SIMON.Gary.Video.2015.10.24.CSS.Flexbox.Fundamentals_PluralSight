e01b5bf970b0b6133e24c1c0b2ada7fda46bbf8d
c00AK <akulik@bluewin.ch>
2015-12-08 23:43:10 +0100

    0115 Finishing the Project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+	$("#magnify").click(function() {
+			$(this).hide();
+			$("#search").toggle("scale"); // jQuery specific
+			$("#search").css("display", "block"); //why needed ?
+	});
 
 });
 
